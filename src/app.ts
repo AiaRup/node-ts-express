f688822e6583096b991f92c53a4dc1edc260baac
Aia Rupsom <aiale88@gmail.com>
2021-03-08 09:49:26 +0200

    boilerplate for using typescript with express and node

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-console.log('hola');
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('hellow');
+});
+
+app.listen(5000, () => console.log('Server running'));
